0d4c39045c1ad303de7bb58f7e63e785684ae1db
wellingtongoulart <wellingtongoulart@outlook.com>
2021-09-26 17:33:38 -0300

    final

--- back.js
+++ back.js
@@ -1,6 +1,27 @@
-function Converter() {
-  var valorElemento = document.getElementById('valor')
-  var valor = valorElemento.value
-  var valorEmDolarNumerico = parseFloat(valor)
-  console.log(valorElemento)
+function ConverterDolar() {
+  var valorDoElemento = document.getElementById('valor')
+  var valor = valorDoElemento.value
+  var valorEmRealNumerico = parseFloat(valor)
+
+  var valorEmDolar = (valorEmRealNumerico * 0.19).toFixed(2)
+
+  if (valorEmDolar > 0) {
+    var elementoValorConvertido = document.getElementById('valorConvertido')
+    var valorConvertido = 'A sua fortuna em Dolar é $' + valorEmDolar
+    elementoValorConvertido.innerHTML = valorConvertido
+  }
+}
+
+function ConverterEuro() {
+  var valorDoElemento = document.getElementById('valor')
+  var valor = valorDoElemento.value
+  var valorEmRealNumerico = parseFloat(valor)
+
+  var valorEmEuro = (valorEmRealNumerico * 0.16).toFixed(2)
+
+  if (valorEmEuro > 0) {
+    var elementoValorConvertido = document.getElementById('valorConvertido')
+    var valorConvertido = 'A sua fortuna em Euro é €' + valorEmEuro
+    elementoValorConvertido.innerHTML = valorConvertido
+  }
 }
