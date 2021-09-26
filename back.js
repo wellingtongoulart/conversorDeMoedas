9bf9ea3aaa342de2dde0f16074bef94c7245269d
wellingtongoulart <wellingtongoulart@outlook.com>
2021-09-26 12:47:09 -0300

    construction

---
+++ back.js
@@ -0,0 +1,6 @@
+function Converter() {
+  var valorElemento = document.getElementById('valor')
+  var valor = valorElemento.value
+  var valorEmDolarNumerico = parseFloat(valor)
+  console.log(valorElemento)
+}
